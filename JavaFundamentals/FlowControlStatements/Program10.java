5206a9db7a5725fa4f1dd7f890a8d592679dd7f5
23bq1a4768-Madhu <23bq1a4768@gmail.com>
2026-07-15 13:09:09 +0530

    Add files via upload

---
+++ JavaFundamentals/FlowControlStatements/Program10.java
@@ -0,0 +1,10 @@
+package FlowControlStatements;
+
+public class Program10 {
+	public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + "\t");
+        }
+    }
+}
---
+++ JavaFundamentals/FlowControlStatements/Program14.java
@@ -0,0 +1,19 @@
+package FlowControlStatements;
+import java.util.Scanner;
+public class Program14 {
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for (; num != 0; num = num / 10) {
+            sum = sum + (num % 10);
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
