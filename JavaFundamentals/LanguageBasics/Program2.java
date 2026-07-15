aa3ee2a07e40a8ab3319b98f67c33610a6d49534
23bq1a4768-Madhu <23bq1a4768@gmail.com>
2026-07-15 13:11:22 +0530

    Add files via upload

---
+++ JavaFundamentals/LanguageBasics/Program2.java
@@ -0,0 +1,10 @@
+package LanguageBasics;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome " + args[0]);
+	}
+
+}
