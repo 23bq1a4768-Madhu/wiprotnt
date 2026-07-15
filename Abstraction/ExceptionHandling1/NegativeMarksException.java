27a5253544d0b854006c57a82aa605416bfa3fae
23bq1a4768-Madhu <23bq1a4768@gmail.com>
2026-07-15 15:27:14 +0530

    Add files via upload

---
+++ Abstraction/ExceptionHandling1/NegativeMarksException.java
@@ -0,0 +1,8 @@
+package ExceptionHandling1;
+
+public class NegativeMarksException extends Exception {
+
+    public NegativeMarksException(String msg) {
+        super(msg);
+    }
+}
---
+++ Abstraction/ExceptionHandling1/OutOfRangeException.java
@@ -0,0 +1,8 @@
+package ExceptionHandling1;
+
+public class OutOfRangeException extends Exception {
+
+    public OutOfRangeException(String msg) {
+        super(msg);
+    }
+}
