757329e6354274cd0ffa44eda585c18adc5c829d
23bq1a4768-Madhu <23bq1a4768@gmail.com>
2026-07-15 13:21:31 +0530

    Add files via upload

---
+++ OOPS/Encapsulation/TestBook.java
@@ -0,0 +1,26 @@
+package Encapsulation;
+
+public class TestBook {
+	public static void main(String[] args) {
+
+        Author author = new Author(
+                "James Gosling",
+                "james@gmail.com",
+                'M');
+
+        Book book = new Book(
+                "Java Programming",
+                author,
+                799.50,
+                25);
+
+        System.out.println("Book Name : " + book.getName());
+        System.out.println("Price : " + book.getPrice());
+        System.out.println("Quantity : " + book.getQtyInStock());
+
+        System.out.println("\nAuthor Details");
+        System.out.println("Name : " + book.getAuthor().getName());
+        System.out.println("Email : " + book.getAuthor().getEmail());
+        System.out.println("Gender : " + book.getAuthor().getGender());
+    }
+}
